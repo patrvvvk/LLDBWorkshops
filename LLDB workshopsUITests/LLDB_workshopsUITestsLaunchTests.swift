0323ca0cd6b867ded80953673c10a2f0f0ff8c04
Patryk Budzinski <contact@budzinski.dev>
2022-10-10 21:24:44 +0200

    Initial Commit

---
+++ LLDB workshopsUITests/LLDB_workshopsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LLDB_workshopsUITestsLaunchTests.swift
+//  LLDB workshopsUITests
+//
+//  Created by Patryk Budzinski on 10/10/2022.
+//
+
+import XCTest
+
+final class LLDB_workshopsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
